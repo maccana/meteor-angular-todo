7fe75a172f091224366fd28dd78ca6a58040d5bb
maccana <mikeymccann@gmail.com>
2015-07-04 16:22:33 -0400

    Add task collection and helper function

--- simple-todos.js
+++ simple-todos.js
@@ -1,12 +1,11 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // counter starts at 0
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
 }
 
