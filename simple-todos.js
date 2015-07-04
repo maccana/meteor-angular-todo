c6b4a0b7f59e3689e4c79c4e48256cf6a92b4090
maccana <mikeymccann@gmail.com>
2015-07-04 16:32:57 -0400

    Update html with task form

--- simple-todos.js
+++ simple-todos.js
@@ -7,6 +7,26 @@ if (Meteor.isClient) {
       return Tasks.find({});
     }
   });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // This function is called when the new task form is submitted
+
+      var text = event.target.text.value;
+
+      // Add new task to mongo collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false; // Prevents page refresh
+    }
+  });
 }
 
 if (Meteor.isServer) {
